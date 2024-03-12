05cad1c6c740c416627bc5ce5f3c1622601480f7
SemperSpirit <kaiten-aiko@mail.ru>
2024-03-12 18:11:38 +0300

    bug fix

--- HomeWork13/Sources/Model.swift
+++ HomeWork13/Sources/Model.swift
@@ -10,10 +10,37 @@ import UIKit
 class Model {
     
     var name: String
-    var imageName: String
+    var systemName: String
 
-    init(name: String, imageName: String) {
+    init(name: String, systemName: String) {
         self.name = name
-        self.imageName = imageName
+        self.systemName = systemName
+    }
+    
+    static func addModels() -> [[Model]] {
+        let arrayOfSettings: [[Model]] = [
+            [
+                Model(name: "Авиарежим", systemName: "1"),
+                Model(name: "Wi-Fi", systemName: "2"),
+                Model(name: "BlueTooth", systemName: "3"),
+                Model(name: "Сотовая связь", systemName: "4"),
+                Model(name: "Режим модема", systemName: "5"),
+                Model(name: "VPN", systemName: "6")
+            ],
+            [
+                Model(name: "Уведомления", systemName: "7"),
+                Model(name: "Звуки, тактильные сигналы", systemName: "8"),
+                Model(name: "Не беспокоить", systemName: "9"),
+                Model(name: "Экранное время", systemName: "10")
+            ],
+            [
+                Model(name: "Основные", systemName: "11"),
+                Model(name: "Пункт управления", systemName: "12"),
+                Model(name: "Экран и яркость", systemName: "13"),
+                Model(name: "Экран 'Домой'", systemName: "14"),
+                Model(name: "Универсальный доступ", systemName: "15")
+            ]
+        ]
+        return arrayOfSettings
     }
 }
--- HomeWork13/Sources/SettingTableViewCell.swift
+++ HomeWork13/Sources/SettingTableViewCell.swift
@@ -4,91 +4,82 @@
 //
 //  Created by Kaiten Aiko on 10.03.2024.
 //
-
-import UIKit
-
 import UIKit
 
 class SettingTableViewCell: UITableViewCell {
     
-    // MARK: - Outlets
+    // MARK: - Outlets -
 
     static let reuseIdentifier = "SettingTableViewCell"
     
-    var model: Model?
+    private var model: Model
 
-    let settingImageView: UIImageView = {
+    private lazy var settingImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
-    let nameLabel: UILabel = {
+    private lazy var nameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let switchControl: UISwitch = {
+    private lazy var switchControl: UISwitch = {
         let switchControl = UISwitch()
-        switchControl.translatesAutoresizingMaskIntoConstraints = false
         return switchControl
     }()
     
+    // MARK: - Inits -
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        self.model = Model(name: "", systemName: "")
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupUI()
+        super.prepareForReuse()
+        accessoryType = .disclosureIndicator
+        accessoryView = nil
+        setupHierarchy()
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    // MARK: - Setup -
     
-    // MARK: - Setup
-    
-    private func setupUI() {
+    private func setupHierarchy() {
         addSubview(settingImageView)
         addSubview(nameLabel)
-        addSubview(switchControl)
-
+    }
+    
+    private func setupLayout() {
         NSLayoutConstraint.activate([
             settingImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             settingImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
             settingImageView.widthAnchor.constraint(equalToConstant: 30),
             settingImageView.heightAnchor.constraint(equalToConstant: 30),
             nameLabel.leadingAnchor.constraint(equalTo: settingImageView.trailingAnchor, constant: 8),
-            nameLabel.trailingAnchor.constraint(equalTo: switchControl.leadingAnchor, constant: -16),
             nameLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
-            switchControl.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            switchControl.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
-
-        switchControl.addTarget(self, action: #selector(switchValueChanged(_:)), for: .valueChanged)
-
-    }
+}
     
-    func setupSwitch() {
-        switchControl.onTintColor = UIColor.green
-        switchControl.isHidden = false
-        switchControl.isOn = false
-        self.switchControl.setOn(true, animated: true)
-    }
-    
-    func removeSwitch() {
-        switchControl.isHidden = true
-    }
-    
-    @objc func switchValueChanged(_ sender: UISwitch) {
-        guard let model = model else { return }
-        print("\(model.name) switch value changed: \(sender.isOn)")
-    }
-
     func configure(with model: Model) {
         self.model = model
-        settingImageView.image = UIImage(named: model.imageName)
+        settingImageView.image = UIImage(named: model.systemName)
         nameLabel.text = model.name
+        
+        if model.name == "Авиарежим" || model.name == "VPN" {
+                setupSwitch()
+            } else {
+                switchControl.removeFromSuperview()
+            }
+    }
+    
+    private func setupSwitch() {
+        accessoryView = switchControl
+        selectionStyle = .none
     }
 }
-
-
